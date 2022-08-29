025daaea663c22467c7870582cd8400556606020
alexxxmf <amoro@alumni.unav.es>
2022-08-29 12:03:42 +0100

    added more stuff to main pair contract

--- contracts/SpaniswapV2Pair.sol
+++ contracts/SpaniswapV2Pair.sol
@@ -4,8 +4,15 @@ pragma solidity ^0.8.9;
 // Import this file to use console.log
 import "hardhat/console.sol";
 import "contracts/ERC20.sol";
+import "contracts/Math.sol";
 
-contract SpaniswapV2Pair is ERC20{
+error InsufficientLiquidityMinted();
+error InsufficientLiquidityBurned();
+error TransferFailed();
+
+contract SpaniswapV2Pair is ERC20, SafeMath{
+  // placeholder, yet to be decided
+  uint256 public MINIMUM_LIQUIDITY = 1000;
   // This is to store reserves for both sides of the pair
   uint256 private reserve0;
   uint256 private reserve1;
@@ -22,7 +29,36 @@ contract SpaniswapV2Pair is ERC20{
   }
 
   function mint() public {
-    
+    uint256 balance0 = ERC20(token0).balanceOf(address(this));
+    uint256 balance1 = ERC20(token1).balanceOf(address(this));
+
+    (uint256 _reserve0, uint256 _reserve1) = getReserves();
+    // How much was received in the last liquidity addition by user
+    uint256 amount0 = balance0 - _reserve0;
+    uint256 amount1 = balance1 - _reserve1;
+
+    uint256 liquidity;
+
+    if (totalSupply == 0) {
+      liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+      // This is a way to keep forever the minimal liquidity in 
+      // an unretrievable position so we garantee am0 * am1 will never
+      // have a 0 on either side destroying K result. is a way to permanently lock that minimum
+      // 0 address https://stackoverflow.com/questions/50580769/what-is-0x0-address-in-solidity-ethereum
+      _mint(address(0), MINIMUM_LIQUIDITY);
+    } else {
+      liquidity = Math.min(
+        (amount0 * totalSupply) / _reserve0,
+        (amount1 * totalSupply) / _reserve1
+      );
+    }
+
+    // regarding revert vs require
+    // https://medium.com/blockchannel/the-use-of-revert-assert-and-require-in-solidity-and-the-new-revert-opcode-in-the-evm-1a3a7990e06e
+    if (liquidity <= 0) revert InsufficientLiquidityMinted();
+
+    _mint()
+
   }
 
   function getReserves()
@@ -34,6 +70,11 @@ contract SpaniswapV2Pair is ERC20{
     )
   {
     return (reserve0, reserve1);
+  }
+
+  function _updateReserves(uint256 _reserve0, uint256 _reserve1) private{
+    reserve0 = _reserve0;
+    reserve1 = _reserve1;
   }
 
 }
